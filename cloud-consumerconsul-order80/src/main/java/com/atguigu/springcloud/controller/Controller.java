3bfa1adbb9cbafe20a3056227d42e31d6fa61d30
zhoujun <1449287613@qq.com>
2020-03-14 17:22:59 +0800

    修改注解

--- cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/controller/Controller.java
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/controller/Controller.java
@@ -5,9 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.Resource;
+
 @RestController
 public class Controller {
-    @Autowired
+    @Resource
     private RestTemplate restTemplate;
     private static final String PAYMENT_URL = "http://cloud-provider-payment";
 
